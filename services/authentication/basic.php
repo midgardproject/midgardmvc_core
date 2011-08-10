33e5c764d19bac8bd033e674bd37dde60fed32f0
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-08-10 18:47:47 +0300

    Make use ['HTTP_AUTHORIZATION'] in case PHP_AUTH_USER and PHP_AUTH_PW are not available

--- services/authentication/basic.php
+++ services/authentication/basic.php
@@ -49,9 +49,19 @@ class midgardmvc_core_services_authentication_basic extends midgardmvc_core_serv
         // TODO: Can this be implemented for Basic auth?
         return;
     }
-    
+
     public function handle_exception(Exception $exception)
     {
+        if ( ! (isset($_SERVER['PHP_AUTH_USER'])
+            && isset($_SERVER['PHP_AUTH_PW']))
+            && isset($_SERVER['HTTP_AUTHORIZATION']))
+        {
+            $auth_params = explode(":", base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+            $_SERVER['PHP_AUTH_USER'] = $auth_params[0];
+            unset($auth_params[0]);
+            $_SERVER['PHP_AUTH_PW'] = implode('', $auth_params);
+        }
+
         if (   !isset($_SERVER['PHP_AUTH_USER'])
             || !isset($_SERVER['PHP_AUTH_PW']))
         {
@@ -74,7 +84,7 @@ class midgardmvc_core_services_authentication_basic extends midgardmvc_core_serv
         if (!$this->login($tokens))
         {
             // Wrong password: Recurse until auth ok or user gives up
-            unset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+            unset($_SERVER['HTTP_AUTHORIZATION'], $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
             $this->handle_exception($exception);
         }
     }
