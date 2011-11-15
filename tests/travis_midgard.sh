03652a92e543840667a1906c744063b4c3208cd8
Henri Bergius <henri.bergius@iki.fi>
2011-11-15 17:15:29 +0200

    'Install' the extension

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -14,4 +14,5 @@ sudo dpkg -i midgard2-common_10.05.5-1_i386.deb
 sudo dpkg -i php5-midgard2_10.05.5-1_i386.deb
 
 # We need to enable Midgard correctly
+cp /usr/lib/php5/20090626/midgard2.so ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626/ 
 echo "extension=midgard2.so" >> ~/.phpfarm/inst/php-5.3.8/lib/php.ini
