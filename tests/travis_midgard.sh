223eda41c96761c398c3d4ec118c4a2e73c4b9ee
Henri Bergius <henri.bergius@iki.fi>
2011-11-15 15:41:31 +0200

    More deb setup hassle

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y php-pear
 sudo pear install http://phptal.org/latest.tar.gz 
 
 # Install Midgard from PPA
-sudo add-apt-repository "deb http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/ ./"
+sudo echo "deb http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/ ./" > /etc/apt/sources.list.d/midgard.list
 sudo apt-get update
 sudo apt-get install -y php5-midgard2
