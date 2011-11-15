78861103907e6fbd15613da08a07110b1a467a60
Henri Bergius <henri.bergius@iki.fi>
2011-11-15 17:24:28 +0200

    Ensure the ext dir is there

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -14,6 +14,7 @@ sudo dpkg -i midgard2-common_10.05.5-1_i386.deb
 sudo dpkg -i php5-midgard2_10.05.5-1_i386.deb
 
 # We need to enable Midgard correctly
+mkdir -p ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626
 EXT_DIR=`php-config5 --extension-dir`
-cp $EXT_DIR/midgard2.so ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626/ 
+cp $EXT_DIR/midgard2.so ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626/midgard2.so
 echo "extension=midgard2.so" >> ~/.phpfarm/inst/php-5.3.8/lib/php.ini
