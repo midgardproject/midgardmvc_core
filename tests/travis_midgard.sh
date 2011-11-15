0ec0837a1ff4f96d9a40ea3aef76c72efb0d8a51
Henri Bergius <henri.bergius@iki.fi>
2011-11-15 17:21:37 +0200

    Correct executable

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -14,6 +14,6 @@ sudo dpkg -i midgard2-common_10.05.5-1_i386.deb
 sudo dpkg -i php5-midgard2_10.05.5-1_i386.deb
 
 # We need to enable Midgard correctly
-EXT_DIR=`php-config --extension-dir`
+EXT_DIR=`php-config5 --extension-dir`
 cp $EXT_DIR/midgard2.so ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626/ 
 echo "extension=midgard2.so" >> ~/.phpfarm/inst/php-5.3.8/lib/php.ini
