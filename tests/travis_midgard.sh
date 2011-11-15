ceee89d458560062fae9fe6fce144e1dec6f0282
Henri Bergius <henri.bergius@iki.fi>
2011-11-15 17:20:26 +0200

    Try to locate the installed extension

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y php-pear
 sudo pear install http://phptal.org/latest.tar.gz 
 
 # Install Midgard from OBS
-sudo apt-get install -y dbus libgda-4.0-4 
+sudo apt-get install -y dbus libgda-4.0-4 php5-dev
 wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/libmidgard2-2010_10.05.5-1_i386.deb
 wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/midgard2-common_10.05.5-1_i386.deb 
 wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/php5-midgard2_10.05.5-1_i386.deb
@@ -14,5 +14,6 @@ sudo dpkg -i midgard2-common_10.05.5-1_i386.deb
 sudo dpkg -i php5-midgard2_10.05.5-1_i386.deb
 
 # We need to enable Midgard correctly
-cp /usr/lib/php5/20090626/midgard2.so ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626/ 
+EXT_DIR=`php-config --extension-dir`
+cp $EXT_DIR/midgard2.so ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626/ 
 echo "extension=midgard2.so" >> ~/.phpfarm/inst/php-5.3.8/lib/php.ini
