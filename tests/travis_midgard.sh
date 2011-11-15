44f8039b8a1aea9dc81a68314a0e95f2e8e66e56
Henri Bergius <henri.bergius@iki.fi>
2011-11-15 15:29:25 +0200

    Test with both Midgard and no Midgard

---
+++ tests/travis_midgard.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install -y php-pear php5-midgard2
+sudo pear install http://phptal.org/latest.tar.gz 
---
+++ tests/travis_nomidgard.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install -y php-pear
+sudo pear install http://phptal.org/latest.tar.gz 
