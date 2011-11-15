60b40b140427b38f5f1684e2d5871884f3cca5ff
Henri Bergius <henri.bergius@iki.fi>
2011-11-15 15:37:10 +0200

    Install Midgard2 PHP extension from OBS

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo apt-get install -y php-pear php5-midgard2
+
+# Install TAL
+sudo apt-get install -y php-pear
 sudo pear install http://phptal.org/latest.tar.gz 
+
+# Install Midgard from PPA
+sudo add-apt-repository "deb http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/ ./"
+sudo apt-get update
+sudo apt-get install -y php5-midgard2
