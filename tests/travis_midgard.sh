f294477303f5d15e33d23bc74a018d71823ac204
Henri Bergius <henri.bergius@iki.fi>
2011-11-16 15:06:32 +0200

    Initial, try to build and install the PHP5 Midgard2 extension

--- tests/travis_midgard.sh
+++ tests/travis_midgard.sh
@@ -3,17 +3,20 @@
 # Install TAL
 pyrus install http://phptal.org/latest.tar.gz 
 
-# Install Midgard from OBS
+# Install Pake
+pyrus channel-discover pear.indeyets.ru
+pyrus install -f http://pear.indeyets.ru/get/pake-1.6.3.tgz
+
+# Install Midgard2 library from OBS
 sudo apt-get install -y dbus libgda-4.0-4 php5-dev
 wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/libmidgard2-2010_10.05.5.1-1_i386.deb
 wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/midgard2-common_10.05.5.1-1_i386.deb 
-wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/php5-midgard2_10.05.5.1-1_i386.deb
+wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/libmidgard2-dev_10.05.5.1-1_i386.deb 
 sudo dpkg -i --force-depends libmidgard2-2010_10.05.5.1-1_i386.deb
 sudo dpkg -i midgard2-common_10.05.5.1-1_i386.deb
-sudo dpkg -i php5-midgard2_10.05.5.1-1_i386.deb
+sudo dpkg -i libmidgard2-dev_10.05.5.1-1_i386.deb
 
-# We need to enable Midgard correctly
-mkdir -p ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626
-EXT_DIR=`php-config5 --extension-dir`
-cp $EXT_DIR/midgard2.so ~/.phpfarm/inst/php-5.3.8/lib/php/extensions/debug-non-zts-20090626/midgard2.so
-echo "extension=midgard2.so" >> ~/.phpfarm/inst/php-5.3.8/lib/php.ini
+# Build and install Midgard2 PHP extension
+wget https://github.com/midgardproject/midgard-php5/tarball/ratatoskr
+tar zxf ratatoskr
+sh -c "cd midgardproject-midgard-php5-*&&`pyrus get php_dir|tail -1`/pake test"
