68324ce508beff06a764ef34f378aedd519a3af0
Henri Bergius <henri.bergius@iki.fi>
2011-11-17 14:18:09 +0200

    Additional safety

--- exceptionhandler.php
+++ exceptionhandler.php
@@ -98,9 +98,11 @@ class midgardmvc_core_exceptionhandler
                 $request = self::bootstrap_request();
             }
             $route = $request->get_route();
-            $errorRoute = clone $route;
-            $errorRoute->template_aliases['root'] = 'midgardmvc-show-error';
-            $request->set_route($errorRoute);
+            if ($route) {
+                $errorRoute = clone $route;
+                $errorRoute->template_aliases['root'] = 'midgardmvc-show-error';
+                $request->set_route($errorRoute);
+            }
                 
             $request->set_data_item('midgardmvc_core_exceptionhandler', $data);
             $request->set_data_item('cache_enabled', false);
